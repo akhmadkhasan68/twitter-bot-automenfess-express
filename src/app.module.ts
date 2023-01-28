55bd65766897015db5bcfefc6d3df6e8c22bb349
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2023-01-28 19:30:05 +0700

    post tweet, install cron package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { DirectMessageController } from "./controllers/direct-message.controller
 import { TwitterClientAuthService } from "./services/twitter-client-auth.service";
 import { TwitterDirectMessageService } from "./services/twitter-direct-message.service";
 import { TwitterApi } from "twitter-api-v2";
+import { TwitterTweetService } from "./services/twitter-tweet.service";
+import { TweetController } from "./controllers/tweet.controller";
 
 
 export default function configureDI(): IDIContainer {
@@ -16,8 +18,14 @@ export default function configureDI(): IDIContainer {
         [TwitterDirectMessageService.name]: object(TwitterDirectMessageService).construct(
             twitterClient
         ),
+        [TwitterTweetService.name]: object(TwitterTweetService).construct(
+            twitterClient
+        ),
         [DirectMessageController.name]: object(DirectMessageController).construct(
             use(TwitterDirectMessageService)
+        ),
+        [TweetController.name]: object(TweetController).construct(
+            use(TwitterTweetService)
         )
     });
 
